1a6ddf32d234fb934fdadba3a7f7f3f7370bc065
Jamie <jamie@jumpingrivers.com>
2019-10-11 13:39:33 +0100

    add load_sample function to package

---
+++ R/load_sample_data.R
@@ -0,0 +1,15 @@
+#' load sample data
+#'
+#' A function to insert the movies and diamonds data into a SQL connection
+#'
+#' @importFrom dplyr copy_to
+#' @param con A connection to a database
+#' @export
+load_sample_data = function(con){
+  e = new.env()
+  data(diamonds, package = 'ggplot2', envir = e)
+  data(movies, package = 'ggplot2movies', envir = e)
+  message('Adding diamonds data to database')
+  dplyr::copy_to(con, e$diamonds, name = 'diamonds' , temporary = FALSE, overwrite = TRUE)
+  dplyr::copy_to(con, e$movies, name = 'movies' , temporary = FALSE, overwrite = TRUE)
+}
